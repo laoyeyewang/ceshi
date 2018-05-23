60afb16b80a2edac58a3adaa7fe2f4a0588a8a55
wang <1339669469@qq.com>
2018-05-23 17:28:48 +0800

    “第一次提交”

---
+++ src/main/java/com/example/wangheng/weiyugateway_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wangheng.weiyugateway_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    webView webview;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
